2ef76763b55cc661ffabac6821a6e15b5ceddfe7
Rohit Shakywar <rohitshakywar016@gmail.com>
2022-12-11 17:01:02 +0530

    some changes appbar

--- src/screens/Home/AddChallenge.tsx
+++ src/screens/Home/AddChallenge.tsx
@@ -2,21 +2,25 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { NavigationComponentProps } from 'react-native-navigation'
 import MySafeArea from '@components/MySafeArea'
+import { Navigator } from '@Navigator'
+import { screenName } from '@screenName'
 
-export interface Props extends NavigationComponentProps {}
+export interface Props extends NavigationComponentProps { }
 
 const AddChallenge: React.FC<Props> = props => {
   return (
     <MySafeArea
-    componentId={props.componentId}
-    isHideBack
-    title="Add Challenge"
-    rightIcon={require('@images/setting-white.png')}>
+      componentId={props.componentId}
+      title="Add Challenge"
+      rightIcon={require('@images/setting-white.png')}
+      rightIconPress={() => Navigator.setPush(props.componentId, screenName.Setting)}
 
-        <ScrollView>
-            <View></View>
-        </ScrollView>
-        </MySafeArea>
+    >
+
+      <ScrollView>
+        <View></View>
+      </ScrollView>
+    </MySafeArea>
   )
 }
 
